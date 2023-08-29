480ecc2c3a1775bcd72c923eefbb694dc79f0da4
tyrrminal <mark@tyrrminal.dev>
2023-08-28 21:48:37 -0400

    validate value transformer value param type

--- lib/Data/Transform/Value.pm
+++ lib/Data/Transform/Value.pm
@@ -9,6 +9,10 @@ class Data::Transform::Value :isa(Data::Transform::Base) {
 
   field $value :param;
 
+  ADJUST {
+    die(ref($value).' is not acceptable for Data::Transform::Value(value)') if(ref($value) && ref($value) ne 'CODE' && ref($value) ne 'Regexp');
+  }
+
   method applies_to($node, $position) {
     return $MATCH_EXACT_VALUE if(!ref($value) && $node eq $value);
     return $MATCH_LIKE_VALUE if(ref($value) eq 'Regexp' && $node =~ /$value/);

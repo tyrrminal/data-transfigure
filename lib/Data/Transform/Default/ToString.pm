28cb284f9375de79b2647ab3eda4b0509cf67405
tyrrminal <mark@tyrrminal.dev>
2023-08-31 16:46:26 -0400

    refactor default stringification into composable class

---
+++ lib/Data/Transform/Default/ToString.pm
@@ -0,0 +1,19 @@
+use v5.26;
+use warnings;
+# ABSTRACT: turns baubles into trinkets
+
+use Object::Pad;
+
+class Data::Transform::Default::ToString : isa(Data::Transform::Default) {
+  
+  sub BUILDARGS($class) {
+    $class->SUPER::BUILDARGS(
+      handler => sub($value) {
+        return "$value"
+      }
+    )
+  }
+
+}
+
+1;

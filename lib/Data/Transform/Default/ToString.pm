892bbfe19cbd89e7a523b3045befacc12362f457
tyrrminal <mark@tyrrminal.dev>
2023-09-01 17:26:25 -0400

    document default::tostring

--- lib/Data/Transform/Default/ToString.pm
+++ lib/Data/Transform/Default/ToString.pm
@@ -1,11 +1,30 @@
 package Data::Transform::Default::ToString;
 use v5.26;
 use warnings;
-# ABSTRACT: turns baubles into trinkets
+
+# ABSTRACT: default stringification of untransformed values
+
+=head1 NAME
+
+Data::Transform::Default::ToString - default stringification of untransformed 
+values
+
+=head1 DESCRIPTION
+
+This transformer unilaterally forces stringification of all the values in a 
+structure that no other transformer applies to, even undef.
+
+=cut
 
 use Object::Pad;
 
 class Data::Transform::Default::ToString : isa(Data::Transform::Default) {
+
+=head1 FIELDS
+
+I<none>
+
+=cut
   
   sub BUILDARGS($class) {
     $class->SUPER::BUILDARGS(
@@ -17,4 +36,36 @@ class Data::Transform::Default::ToString : isa(Data::Transform::Default) {
 
 }
 
+=pod
+
+=head1 AUTHOR
+
+Mark Tyrrell C<< <mtyrrell@cpan.org> >>
+
+=head1 LICENSE
+
+Copyright (c) 2023 Mark Tyrrell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+=cut
+
 1;
+
+__END__

2d28c55de0f03a4b76626eea069a99737d78410e
tyrrminal <mark@tyrrminal.dev>
2023-09-01 17:54:13 -0400

    document lowerCamelKeys and uppercaseHashKeyIDSuffix transformers

--- lib/Data/Transform/PostProcess/LowerCamelKeys.pm
+++ lib/Data/Transform/PostProcess/LowerCamelKeys.pm
@@ -1,7 +1,22 @@
 package Data::Transform::PostProcess::LowerCamelKeys;
 use v5.26;
 use warnings;
-# ABSTRACT: turns baubles into trinkets
+
+# ABSTRACT: converts hash keys to lowerCamelCase
+
+=head1 NAME
+
+Data::Transform::PostProcess::LowerCamelKeys - converts hash keys to 
+lowerCamelCase
+
+=head1 DESCRIPTION
+
+C<Data::Transform::PostProcess::LowerCamelKeys> is intended for cases where the
+backend policies require C<snake_case> but the frontend (and API) policies 
+dictate C<camelCase>. As a post-process transformer, adding it rewrites all of 
+the structure's hash keys to the proper format in that scenario.
+
+=cut
 
 use Object::Pad;
 
@@ -9,6 +24,12 @@ use Data::Transform::PostProcess;
 class Data::Transform::PostProcess::LowerCamelKeys : isa(Data::Transform::PostProcess) {
   use Data::Transform         qw(hk_rewrite_cb);
   use String::CamelSnakeKebab qw(lower_camel_case);
+
+=head1 FIELDS
+
+I<none>
+
+=cut
 
   sub BUILDARGS ($class) {
     $class->SUPER::BUILDARGS(
@@ -20,4 +41,36 @@ class Data::Transform::PostProcess::LowerCamelKeys : isa(Data::Transform::PostPr
 
 }
 
+=pod
+
+=head1 AUTHOR
+
+Mark Tyrrell C<< <mtyrrell@cpan.org> >>
+
+=head1 LICENSE
+
+Copyright (c) 2023 Mark Tyrrell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+=cut
+
 1;
+
+__END__
